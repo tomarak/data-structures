6e093a8abdb28f69271f506868c1c81fb6e5f09b
tomarak <ankuto@gmail.com>
2015-03-25 16:38:41 -0700

    Working on queue.js with shared functional instantiation

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,45 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {
+    storage : {},
+    storageSize : 0
+  };
+
+
+  _.extend(someInstance, stackMethods);
+
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
 
+  push: function(value){
+    this.storage[this.storageSize] = value;
+    this.storageSize++;
+
+  },
+
+  pop: function(){
+    var result;
+    if(this.storageSize > 0){
+      result = this.storage[this.storageSize - 1];
+      delete this.storage[this.storageSize - 1];
+
+      _.each(this.storage, function(val, key){
+        if(key !== "0"){
+          this.storage[key - 1] = val;
+        }
+      });
+
+      this.storageSize--;
+    }
+    return result;
+  },
+
+  size: function(){
+    return this.storageSize;
+  }
+
+};
 
