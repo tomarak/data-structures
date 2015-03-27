753ef9bfa057091a24f4f9aaf9d644dd28c4481b
tomarak <ankuto@gmail.com>
2015-03-26 22:16:14 -0700

    tree in progress

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,24 +1,32 @@
 var Tree = function(value){
   var newTree = {};
   newTree.value = value;
+  _.extend(newTree, treeMethods);
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
   return newTree;
 };
 
-
-
-
-
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  var child = Tree(value);
+  this.children.push(child);
 };
 
 treeMethods.contains = function(target){
+  if(this.value && this.value === target){
+    return true;
+  }
+  for(var i = 0; i < this.children.length; i++){
+    console.log(this.value);
+    var newnode = this.children[i];
+    return newnode.contains(target);
+  }
+
+  return false;
 
 };
 
@@ -26,3 +34,10 @@ treeMethods.contains = function(target){
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+// var tree1 = Tree(4);
+// tree1.addChild(5);
+// tree1.addChild(6);
+// console.log("tree1: ", JSON.stringify(tree1));
+// console.log(tree1.children[0]);
+
