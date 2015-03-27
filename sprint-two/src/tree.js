ae0d855649d16b9a3ddbf007d3d9240fd094a65c
tomarak <ankuto@gmail.com>
2015-03-27 15:31:59 -0700

    Completed tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -17,17 +17,15 @@ treeMethods.addChild = function(value){
 };
 
 treeMethods.contains = function(target){
-  if(this.value && this.value === target){
+  if(this.value === target){
     return true;
   }
   for(var i = 0; i < this.children.length; i++){
-    console.log(this.value);
-    var newnode = this.children[i];
-    return newnode.contains(target);
+    if (this.children[i].contains(target)) {
+      return true;
+    }
   }
-
   return false;
-
 };
 
 
@@ -35,9 +33,15 @@ treeMethods.contains = function(target){
  * Complexity: What is the time complexity of the above functions?
  */
 
-// var tree1 = Tree(4);
-// tree1.addChild(5);
-// tree1.addChild(6);
-// console.log("tree1: ", JSON.stringify(tree1));
-// console.log(tree1.children[0]);
-
+var tree1 = Tree(1);
+tree1.addChild(2);
+tree1.addChild(3);
+tree1.children[0].addChild(4);
+tree1.children[0].addChild(5);
+tree1.children[0].addChild(6);
+console.log("tree1: ", JSON.stringify(tree1));
+console.log(tree1.contains(2));
+console.log(tree1.contains(3));
+console.log(tree1.contains(4));
+console.log(tree1.contains(5));
+console.log(tree1.contains(6));
